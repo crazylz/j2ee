37ad1a02b779b4e2ed6126fe8d5c9769cbb43f27
GhoulTouka <493072591@qq.com>
2019-06-12 15:13:04 +0800

    完善了已购产品

---
+++ src/pages/user/existingloans.vue
@@ -0,0 +1,215 @@
+<template>
+  <div>
+  <el-table
+    :data="tableData"
+    height=896%
+    style="width: 88%">
+    <el-table-column
+      label="金额"
+      width="180">
+      <template slot-scope="scope">
+        <span>￥{{ scope.row.amount }}</span>
+      </template>
+    </el-table-column>
+
+    <el-table-column
+      label="提交时间"
+      width="180">
+      <template slot-scope="scope">
+        <i class="el-icon-time"></i>
+        <span style="margin-left: 10px">{{ scope.row.date }}</span>
+      </template>
+    </el-table-column>
+
+    <el-table-column
+      label="姓名"
+      width="180">
+      <template slot-scope="scope">
+        <el-popover trigger="hover" placement="top">
+          <p>姓名: {{ scope.row.name }}</p>
+          <p>住址: {{ scope.row.address }}</p>
+          <div slot="reference" class="name-wrapper">
+            <el-tag size="medium">{{ scope.row.name }}</el-tag>
+          </div>
+        </el-popover>
+      </template>
+    </el-table-column>
+
+    <el-table-column label="操作">
+      <template slot-scope="scope">
+        <el-button
+          size="mini"
+          @click="handleEdit(scope.$index, scope.row)">购买</el-button>
+      </template>
+    </el-table-column>
+    
+  </el-table>
+
+  <el-pagination
+  style = "position: absolute;
+           left:50%;
+           transform:tranfromX(-50%);
+           margin-top: -40px;"
+    @size-change="handleSizeChange"
+    @current-change="handleCurrentChange"
+    :page-size="100"
+    layout="prev, pager, next, jumper"
+    :total="100*(all_tableData.length/16)">
+  </el-pagination>
+
+
+  </div>
+</template>
+
+
+
+<script>
+  export default {
+    data() {
+      return {
+        all_tableData: [{
+          amount: 1000000,
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          amount: 1000000,
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          amount: 1000000,
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          amount: 1000000,
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          amount: 1000000,
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          amount: 1000000,
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          amount: 1000000,
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          amount: 1000000,
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          amount: 1000000,
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          amount: 1000000,
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          amount: 1000000,
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          amount: 1000000,
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          amount: 1000000,
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          amount: 1000000,
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          amount: 1000000,
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          amount: 1000000,
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          amount: 1000000,
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          amount: 1000000,
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          amount: 1000000,
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }],
+        tableData: []
+      }
+    },
+    methods: {
+      handleEdit(index, row) {
+        console.log(index, row);
+      },
+
+      getDataByPage(pageindex){
+        var begin = pageindex * 16;
+        if(begin > this.all_tableData.length){
+          this.tableData = this.all_tableData.slice(begin-16, this.all_tableData.length);
+        }
+        else{
+          this.tableData = this.all_tableData.slice(begin-16, begin);
+        }
+        // console.log(begin);
+      },
+
+      handleSizeChange(val) {
+        console.log(`每页 ${val} 条`);
+      },
+
+      handleCurrentChange(val) {
+        this.getDataByPage(val);
+      },
+      
+      getOriginalData(){
+        if(this.all_tableData.length < 16){
+          this.tableData = this.all_tableData.slice(0, this.all_tableData.length);
+        }
+        else{
+          this.tableData = this.all_tableData.slice(0, 16);
+        }
+      }
+    },
+
+    mounted(){
+      this.getOriginalData();
+    }
+  }
+</script>
+
+<style>
+.el-table{
+  position: absolute;
+  top: 60px;
+  left:200px;
+}
+</style>
