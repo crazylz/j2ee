d70c84af8f697202c252ea9027c8056398b66a3f
GhoulTouka <493072591@qq.com>
2019-06-25 11:10:49 +0800

    调整了下样式

--- src/pages/user/credit.vue
+++ src/pages/user/credit.vue
@@ -8,7 +8,7 @@
 
 <el-form ref="form" :model="form" label-position="top">
   <el-form-item label="负债金额">
-    <el-input v-model="form.name" placeholder="无负债则填写0"></el-input>
+    <el-input v-model="form.name" placeholder="无负债则填写0" style="width: 25%"></el-input>
   </el-form-item>
 
   <el-form-item label="房产">
@@ -21,7 +21,7 @@
     </el-select>
   </el-form-item>
 
-  <el-form-item label="活动时间">
+  <!-- <el-form-item label="活动时间">
     <el-col :span="11">
       <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
     </el-col>
@@ -29,20 +29,20 @@
     <el-col :span="11">
       <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
     </el-col>
-  </el-form-item>
+  </el-form-item> -->
 
-  <el-form-item label="即时配送">
+  <!-- <el-form-item label="即时配送">
     <el-switch v-model="form.delivery"></el-switch>
-  </el-form-item>
+  </el-form-item> -->
 
-  <el-form-item label="活动性质">
+  <!-- <el-form-item label="活动性质">
     <el-checkbox-group v-model="form.type">
       <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
       <el-checkbox label="地推活动" name="type"></el-checkbox>
       <el-checkbox label="线下主题活动" name="type"></el-checkbox>
       <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
     </el-checkbox-group>
-  </el-form-item>
+  </el-form-item> -->
 
   <el-form-item label="配偶是否有工作">
     <el-radio-group v-model="form.spouse">
@@ -51,9 +51,10 @@
     </el-radio-group>
   </el-form-item>
 
-  <el-form-item label="活动形式">
+  <!-- <el-form-item label="活动形式">
     <el-input type="textarea" v-model="form.desc"></el-input>
-  </el-form-item>
+  </el-form-item> -->
+
   <el-form-item>
     <el-button type="primary" @click="onSubmit">提交</el-button>
     <el-button>取消</el-button>
@@ -97,5 +98,8 @@ import {post, get} from '../../request/http.js'
 </script>
 
 <style scoped>
-  
+  .el-form{
+    text-align:left;
+    width: 60%;
+  }
 </style>
