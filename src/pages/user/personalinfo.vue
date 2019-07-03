8629f2c915a1abaad92183ce979d22d706300a97
GhoulTouka <493072591@qq.com>
2019-07-03 12:08:55 +0800

    个人资料

--- src/pages/user/personalinfo.vue
+++ src/pages/user/personalinfo.vue
@@ -6,6 +6,25 @@
     <el-breadcrumb-item :to="{ path: '/userhome/personalinfo' }">个人资料</el-breadcrumb-item>
   </el-breadcrumb>
 
+
+  <el-dialog :visible.sync='editpassword'>
+    <h2 style="margin-top:-30px;text-align: center;color: #606266; font-size:30px">支付密码</h2>
+
+    <el-form style="margin-right:120px" label-width='200px'>
+      <el-form-item style="margin-left:-44px" label='原始密码'>
+        <el-input v-model='user.password' placeholder='请输入原始密码' clearable></el-input>
+      </el-form-item>
+
+      <el-form-item style="margin-left:-44px" label='支付密码'>
+        <el-input v-model='user.password' placeholder='请输入支付密码' clearable></el-input>
+      </el-form-item>
+
+      <el-form-item style="margin-left:-80px;margin-top:10px">
+        <el-button type='primary'>确认</el-button>
+      </el-form-item>
+    </el-form>   
+  </el-dialog>
+
        <div class="card-div">
       <div
         style="font-weigth:bold; font-size: 20px; float: left; margin-left: 10px; margin-top: 20px">| 基本资料</div>
@@ -50,17 +69,12 @@
 
         <el-form-item label="工龄：" style="text-align:left">
           <label v-if="editable == false">{{user.lengthOfService}}年</label>
-          <el-input v-model="user.lengthOfService" v-else></el-input>
+          <el-input v-model="user.lengthOfService" v-else disabled></el-input>
         </el-form-item> 
 
         <el-form-item label="工资：" style="text-align:left">
           <label v-if="editable == false">￥{{user.salary}}</label>
-          <el-input v-model="user.salary" v-else></el-input>
-        </el-form-item>
-
-        <el-form-item label="第三方账户号码：" style="text-align:left">
-          <label v-if="editable == false">{{user.bankAccount}}</label>
-          <el-input v-model="user.bankAccount" v-else></el-input>
+          <el-input v-model="user.salary" v-else disabled></el-input>
         </el-form-item>
 
         <el-form-item label="身份证号码：" style="text-align:left">
@@ -68,31 +82,54 @@
           <el-input v-model="user.idCardNumber" v-else disabled></el-input>
         </el-form-item>
 
+
         <!-- 中间加条横线 -->
         <div style="background:#afaaaa; height:1px; margin-left: 100px; margin-right: 50px; margin-bottom:25px"/>
 
         <el-form-item label="失信次数：" style="text-align:left">
           <label v-if="editable == false">{{user.discreditedRecords}}</label>
-          <el-input v-model="user.discreditedRecords" v-else></el-input>
+          <el-input v-model="user.discreditedRecords" v-else disabled></el-input>
         </el-form-item>
 
         <el-form-item label="信用评级：" style="text-align:left">
           <label v-if="editable == false">{{user.rank}}</label>
-          <el-input v-model="user.rank" v-else></el-input>
+          <el-input v-model="user.rank" v-else disabled></el-input>
         </el-form-item>
 
-        <el-form-item label="银行卡号：" style="text-align:left">
-          <label v-if="editable == false">{{user.bankAccount}}</label>
-          <el-input v-model="user.bankAccount" v-else></el-input>
-        </el-form-item>
+      
 
         <el-form-item>
           <el-button type="info" round @click="editable = true" v-if="editable == false">编辑</el-button>
           <div v-else>
-          <el-button type="danger" round @click="editable = false">取消编辑</el-button>
+          <el-button type="danger" round @click="editable = false;getUser()">取消编辑</el-button>
           <el-button type="success" round @click="handleDetail">保存</el-button>
           </div>
         </el-form-item>
+
+      </el-form>
+    </div>
+
+
+    <div class="card-div" style="margin-top: 10px">
+      <div
+        style="font-weigth:bold; font-size: 20px; float: left; margin-left: 10px; margin-top: 20px;"
+      >| 支付模块</div>
+
+      <el-form ref="base-form" label-position="right" label-width="200px" style="padding-top: 80px;">
+      <el-form-item label="银行卡号：" style="text-align:left">
+          <label v-if="editable == false">{{user.bankAccount}}</label>
+          <el-input v-model="user.bankAccount" v-else disabled></el-input>
+      </el-form-item>
+
+      <el-form-item label="第三方账户号码：" style="text-align:left">
+          <label v-if="editable == false">{{user.paymentAccount}}</label>
+          <el-input v-model="user.paymentAccount" v-else disabled></el-input>
+      </el-form-item>
+
+
+      <el-form-item label="支付密码：" style="text-align:left">
+      <el-button type="primary" round @click="editpassword = true">设置支付密码</el-button>
+      </el-form-item>
 
       </el-form>
     </div>
@@ -119,10 +156,12 @@ import {post, get, post2} from '../../request/http.js'
         idCardNumber:null,
         discreditedRecords:null,
         rank:null,
-        userId:null
+        userId:null,
+        password:null
         },
 
         editable: false,
+        editpassword: false,
 
       detailrules:{
       name: [
@@ -217,6 +256,10 @@ import {post, get, post2} from '../../request/http.js'
       })
 
     },
+
+    handlePassword(){
+
+    }
 
     },
 
