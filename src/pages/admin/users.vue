148b2240bfa55c0edf876baaed0d67bdb830f176
XuJiaLe1997 <1226501329@qq.com>
2019-07-05 17:14:53 +0800

    Update users.vue

--- src/pages/admin/users.vue
+++ src/pages/admin/users.vue
@@ -1,218 +1,177 @@
 <template>
   <div>
-    <el-breadcrumb separator="/" style="postion:absolute;left:20px;top:20px;margin-bottom:30px;font-size:18px;">
+    <el-breadcrumb
+      separator="/"
+      style="postion:absolute;left:20px;top:20px;margin-bottom:30px;font-size:18px;"
+    >
       <el-breadcrumb-item :to="{ path: '/adminhome' }">管理员</el-breadcrumb-item>
       <el-breadcrumb-item :to="{ path: '/adminhome/users' }">管理用户</el-breadcrumb-item>
     </el-breadcrumb>
 
-    <el-table
-      ref="filterTable"
-      :data="all_tableData.slice(pageIndex*10-10, pageIndex*10)"
-      border>
-      <el-table-column
-        label="用户id"
-        align="center"
-        sortable
-        prop="userId">
+    <el-table ref="filterTable" :data="all_tableData.slice(pageIndex*10-10, pageIndex*10)" border>
+      <el-table-column label="用户id" align="center" sortable prop="userId">
         <template slot-scope="scope">
           <span>{{scope.row.userId}}</span>
         </template>
       </el-table-column>
 
-      <el-table-column
-        label="身份证"
-        align="center">
+      <el-table-column label="身份证" align="center">
         <template slot-scope="scope">
           <span>{{scope.row.idCardNumber}}</span>
         </template>
       </el-table-column>
 
-      <el-table-column
-        label="手机号码"
-        align="center">
+      <el-table-column label="手机号码" align="center">
         <template slot-scope="scope">
           <span>{{scope.row.phoneNumber}}</span>
         </template>
       </el-table-column>
 
-      <el-table-column
-        label="性别"
-        align="center"
-        sortable
-        prop="gender">
+      <el-table-column label="性别" align="center" sortable prop="gender">
         <template slot-scope="scope">
           <span>{{getGender(scope.row.gender)}}</span>
         </template>
       </el-table-column>
 
-      <el-table-column
-        label="第三方支付帐号"
-        align="center">
+      <el-table-column label="第三方支付帐号" align="center">
         <template slot-scope="scope">
           <span>{{scope.row.paymentAccount}}</span>
         </template>
       </el-table-column>
 
-      <el-table-column
-        label="银行卡帐号"
-        align="center">
+      <el-table-column label="银行卡帐号" align="center">
         <template slot-scope="scope">
           <span>{{scope.row.bankAccount}}</span>
         </template>
       </el-table-column>
 
-      <el-table-column
-        label="失信次数"
-        align="center"
-        sortable
-        prop="discreditedRecords">
+      <el-table-column label="失信次数" align="center" sortable prop="discreditedRecords">
         <template slot-scope="scope">
           <span>{{scope.row.discreditedRecords}}</span>
         </template>
       </el-table-column>
 
-      <el-table-column
-        label="工资"
-        align="center"
-        sortable
-        prop="salary">
+      <el-table-column label="工资" align="center" sortable prop="salary">
         <template slot-scope="scope">
           <span>{{scope.row.salary}}</span>
         </template>
       </el-table-column>
 
-      <el-table-column
-        label="信用评级"
-        align="center"
-        sortable
-        prop="rank">
+      <el-table-column label="信用评级" align="center" sortable prop="rank">
         <template slot-scope="scope">
           <span>{{scope.row.rank}}</span>
         </template>
       </el-table-column>
 
-      <el-table-column
-        label="姓名"
-        align="center">
+      <el-table-column label="姓名" align="center">
         <template slot-scope="scope">
           <span>{{scope.row.name}}</span>
         </template>
       </el-table-column>
 
-      <el-table-column
-        label="工龄"
-        align="center"
-        sortable
-        prop="lengthOfService">
+      <el-table-column label="工龄" align="center" sortable prop="lengthOfService">
         <template slot-scope="scope">
           <span>{{scope.row.lengthOfService}}</span>
         </template>
       </el-table-column>
 
-
       <el-table-column label="操作" align="center">
         <template slot-scope="scope">
-          <el-button
-            size="mini"
-            type="danger"
-            @click="handleEdit(scope.row)">删除用户</el-button>
+          <el-button size="mini" type="danger" @click="handleEdit(scope.row)">删除用户</el-button>
         </template>
       </el-table-column>
-    
-   </el-table>
+    </el-table>
 
     <el-pagination
       @size-change="handleSizeChange"
       @current-change="handleCurrentChange"
       :page-size="100"
       layout="prev, pager, next, jumper"
-      :total="100*(all_tableData.length/10)">
-    </el-pagination>
-
-
+      :total="100*(all_tableData.length/10)"
+    ></el-pagination>
   </div>
 </template>
 
 
 
 <script>
-import {post, get} from '../../request/http.js'
-  export default {
-    data() {
-      return {
-        all_tableData: [],
-        pageIndex:1,
-      }
-    },
-    methods: {
-      handleEdit(row) {
-        var res = post("/api/admin/deleteAccount", {id:row.userId});
-        res.then(result=>{
-          if(result.code == 0){
+import { post, get } from "../../request/http.js";
+export default {
+  data() {
+    return {
+      all_tableData: [],
+      pageIndex: 1
+    };
+  },
+  methods: {
+    handleEdit(row) {
+      this.$confirm("确认删除该用户？", "确认信息", {
+        distinguishCancelAndClose: true,
+        confirmButtonText: "确定",
+        cancelButtonText: "取消"
+      }).then(() => {
+        var res = post("/api/admin/deleteAccount", { id: row.userId });
+        res.then(result => {
+          if (result.code == 0) {
             this.$msgbox({
-            title: '提示',
-            message: result.msg,
-            type: 'success'
+              title: "提示",
+              message: result.msg,
+              type: "success"
             });
             var res1 = get("/api/admin/users", {});
-            res1.then(user=>{
+            res1.then(user => {
               this.all_tableData = user.data;
-            }
-      )
-          }
-          else{
+            });
+          } else {
             this.$msgbox({
-            title: '提示',
-            message: result.msg,
-            type: 'error'
+              title: "提示",
+              message: result.msg,
+              type: "error"
             });
           }
-
-        })
-      },
-
-
-      handleSizeChange(val) {
-        console.log(`每页 ${val} 条`);
-      },
-
-      handleCurrentChange(val) {
-        this.pageIndex=val;
-      },
-      
-      filterHandler(value, row, column) {
-        const property = column['property'];
-        return row[property] === value;
-      },
-      getGender(state){
-        if(state == 0){
-          return '未设置';
-        }
-        else{
-          return state == 1 ? '男' : '女';
-        }
-      },
+        });
+      });
     },
 
-    mounted(){
-      var res = get("/api/admin/users", {});
-      res.then(user=>{
-        this.all_tableData = user.data;
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+    },
+
+    handleCurrentChange(val) {
+      this.pageIndex = val;
+    },
+
+    filterHandler(value, row, column) {
+      const property = column["property"];
+      return row[property] === value;
+    },
+    getGender(state) {
+      if (state == 0) {
+        return "未设置";
+      } else {
+        return state == 1 ? "男" : "女";
       }
-      )
     }
+  },
+
+  mounted() {
+    var res = get("/api/admin/users", {});
+    res.then(user => {
+      this.all_tableData = user.data;
+    });
   }
+};
 </script>
 
 <style scoped>
-.el-table{
+.el-table {
   /* display:inline-block;
   text-align:center; */
   width: 100%;
   /* width:fit-content;
   margin:auto; */
 }
-.el-table.column{
-  width:9.09%;
+.el-table.column {
+  width: 9.09%;
 }
 </style>
