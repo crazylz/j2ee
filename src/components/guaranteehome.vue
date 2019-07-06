c006f052e58cbd123b1db8b7660c9c70dcc48d13
XuJiaLe1997 <1226501329@qq.com>
2019-07-06 19:52:34 +0800

    统一界面的header部分logo

--- src/components/guaranteehome.vue
+++ src/components/guaranteehome.vue
@@ -3,47 +3,57 @@
     <!-- 需要将元素设置当一个容器里面 -->
     <el-container class="container">
       <!-- 顶栏 -->
-      <el-header >
-        <span  class="system-name">{{systemName}}</span>
-        <span class="bell" @click="bell()"><big><i class="el-icon-message-solid"></i></big></span>
+      <el-header>
+         <span class="system-name" style="font-size:28px">
+          <img src="../assets/logo.png" style="width:25px" />
+          时不我贷 
+          <span style="font-size:14px">P2P小额贷款平台 {{systemName}}</span>
+        </span>
+        <span class="bell" @click="bell()">
+          <big>
+            <i class="el-icon-message-solid"></i>
+          </big>
+        </span>
         <div class="guarantee">
           <el-dropdown>
             <span style="color: white">
-              <big><i class="el-icon-user-solid"></i></big>
+              <big>
+                <i class="el-icon-user-solid"></i>
+              </big>
               {{adminName}}
             </span>
             <el-dropdown-menu slot="dropdown">
               <a href="#/login">
-              <el-dropdown-item icon="el-icon-unlock">登出</el-dropdown-item>
+                <el-dropdown-item icon="el-icon-unlock">登出</el-dropdown-item>
               </a>
             </el-dropdown-menu>
           </el-dropdown>
         </div>
       </el-header>
-        
-        <!-- 需要将侧栏和主页面设置当一个容器里面 -->
-        <el-container>
-          <!-- 侧栏 -->
-          <el-aside width="200px">
-            <el-menu :default-active="$route.path" router unique-opened >
-              <el-menu-item index="/guaranteehome/guarantee" style="text-align:left" class="item">
+
+      <!-- 需要将侧栏和主页面设置当一个容器里面 -->
+      <el-container>
+        <!-- 侧栏 -->
+        <el-aside width="200px">
+          <el-menu :default-active="$route.path" router unique-opened>
+            <el-menu-item index="/guaranteehome/guarantee" style="text-align:left" class="item">
               <i class="el-icon-s-custom"></i>担保请求
-              </el-menu-item>
+            </el-menu-item>
 
-               <el-menu-item index="/guaranteehome/freeze" style="text-align:left" class="item">
+            <el-menu-item index="/guaranteehome/freeze" style="text-align:left" class="item">
               <i class="el-icon-s-order"></i>处理逾期记录
-               </el-menu-item>
-            </el-menu>
-          </el-aside>
+            </el-menu-item>
+          </el-menu>
+        </el-aside>
 
-          <el-main>
-            <transition name="fade" mode="out-in">
-              <router-view></router-view>
-            </transition>
-          </el-main>
-        </el-container>
+        <el-main>
+          <transition name="fade" mode="out-in">
+            <router-view></router-view>
+          </transition>
+        </el-main>
+      </el-container>
     </el-container>
-    </div>
+  </div>
 </template>
 
 
@@ -51,69 +61,66 @@
 <script>
 let data = () => {
   return {
-    systemName: '担保人界面',
-    adminName: '担保人'
-  }
-}
+    systemName: "担保人界面",
+    adminName: "担保人"
+  };
+};
 
 export default {
   data: data,
   methods: {
-    bell:function(){
-        this.$router.push({path:'/guaranteehome/guaranteeinformation'});
-    },
+    bell: function() {
+      this.$router.push({ path: "/guaranteehome/guaranteeinformation" });
+    }
   },
-  mounted: function() {
-    
-  }
-}
+  mounted: function() {}
+};
 </script>
 
 <style scoped="scoped">
 /* 设置min-height调节最小高度来实现样式统一 */
-.el-header{
-    background-color: #1b2226;
-    color: rgb(255, 255, 255);
-    text-align: center;
-    line-height: 60px;
-  }
-  
-  .el-aside {
-    background-color: #272a2d;
-    color: #333;
-    /* background-image: url('../assets/back.jpeg'); */
-    text-align: center;
-    line-height: 200px;
-    min-height: 90vh;
-  }
-  
-  .el-main {
-    background-color:  #E9EEF3;
-    color: #333;
-    text-align: center;
-    line-height: 30px;
-    min-height: 90vh;
-  }
+.el-header {
+  background-color: #1b2226;
+  color: rgb(255, 255, 255);
+  text-align: center;
+  line-height: 60px;
+}
 
-  .item {
-    background-color: #272a2d;
-    color: white;
-  }
-  .system-name{
-    float:left;
-    margin-left:70px;
-    font-size:20px;
-  }
-  
-  .bell{
-    float:right;
-    margin-right:70px;
-  } 
-   
-   
-  .guarantee{
-     font-size:18px;
-    float:right;
-    margin-right:30px;
-  }
+.el-aside {
+  background-color: #272a2d;
+  color: #333;
+  /* background-image: url('../assets/back.jpeg'); */
+  text-align: center;
+  line-height: 200px;
+  min-height: 90vh;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 30px;
+  min-height: 90vh;
+}
+
+.item {
+  background-color: #272a2d;
+  color: white;
+}
+.system-name {
+  float: left;
+  margin-left: 70px;
+  font-size: 20px;
+}
+
+.bell {
+  float: right;
+  margin-right: 70px;
+}
+
+.guarantee {
+  font-size: 18px;
+  float: right;
+  margin-right: 30px;
+}
 </style>
--- src/components/reviewerhome.vue
+++ src/components/reviewerhome.vue
@@ -3,42 +3,52 @@
     <!-- 需要将元素设置当一个容器里面 -->
     <el-container class="container">
       <!-- 顶栏 -->
-      <el-header >
-        <span  class="system-name">{{systemName}}</span>
-        <span class="bell" @click="bell()"><big><i class="el-icon-message-solid"></i></big></span>
+      <el-header>
+         <span class="system-name" style="font-size:28px">
+          <img src="../assets/logo.png" style="width:25px" />
+          时不我贷 
+          <span style="font-size:14px">P2P小额贷款平台 {{systemName}}</span>
+        </span>
+        <span class="bell" @click="bell()">
+          <big>
+            <i class="el-icon-message-solid"></i>
+          </big>
+        </span>
         <div class="guarantee">
           <el-dropdown>
             <span style="color: white">
-              <big><i class="el-icon-user-solid"></i></big>
+              <big>
+                <i class="el-icon-user-solid"></i>
+              </big>
               {{adminName}}
             </span>
             <el-dropdown-menu slot="dropdown">
               <a href="#/login">
-              <el-dropdown-item icon="el-icon-unlock">登出</el-dropdown-item>
+                <el-dropdown-item icon="el-icon-unlock">登出</el-dropdown-item>
               </a>
             </el-dropdown-menu>
           </el-dropdown>
         </div>
       </el-header>
-    
-        <!-- 需要将侧栏和主页面设置当一个容器里面 -->
-    <el-container>
-          <el-aside width="200px">
-            <el-menu :default-active="$route.path" router unique-opened >
-              <el-menu-item index="/reviewerhome/review" style="text-align:left" class="item">
-              <i class="el-icon-s-custom"></i>审核
-              </el-menu-item>
-            </el-menu>
-          </el-aside>
 
-          <el-main>
-            <transition name="fade" mode="out-in">
-              <router-view></router-view>
-            </transition>
-          </el-main>
-        </el-container>
+      <!-- 需要将侧栏和主页面设置当一个容器里面 -->
+      <el-container>
+        <el-aside width="200px">
+          <el-menu :default-active="$route.path" router unique-opened>
+            <el-menu-item index="/reviewerhome/review" style="text-align:left" class="item">
+              <i class="el-icon-s-custom"></i>审核
+            </el-menu-item>
+          </el-menu>
+        </el-aside>
+
+        <el-main>
+          <transition name="fade" mode="out-in">
+            <router-view></router-view>
+          </transition>
+        </el-main>
+      </el-container>
     </el-container>
-    </div>
+  </div>
 </template>
 
 
@@ -46,69 +56,66 @@
 <script>
 let data = () => {
   return {
-    systemName: '审核员界面',
-    adminName: '审核员'
-  }
-}
+    systemName: "审核员界面",
+    adminName: "审核员"
+  };
+};
 
 export default {
   data: data,
   methods: {
-    bell:function(){
-        this.$router.push({path:'/reviewerhome/reviewerinformation'});
-    },
+    bell: function() {
+      this.$router.push({ path: "/reviewerhome/reviewerinformation" });
+    }
   },
-  mounted: function() {
-    
-  }
-}
+  mounted: function() {}
+};
 </script>
 
 <style scoped="scoped">
 /* 设置min-height调节最小高度来实现样式统一 */
-.el-header{
-    background-color: #1b2226;
-    color: rgb(255, 255, 255);
-    text-align: center;
-    line-height: 60px;
-  }
-  
-  .el-aside {
-    background-color: #272a2d;
-    color: #333;
-    /* background-image: url('../assets/back.jpeg'); */
-    text-align: center;
-    line-height: 200px;
-    min-height: 90vh;
-  }
-  
-  .el-main {
-    background-color:  #E9EEF3;
-    color: #333;
-    text-align: center;
-    line-height: 30px;
-    min-height: 90vh;
-  }
+.el-header {
+  background-color: #1b2226;
+  color: rgb(255, 255, 255);
+  text-align: center;
+  line-height: 60px;
+}
 
-  .item {
-    background-color: #272a2d;
-    color: white;
-  }
-  .system-name{
-    float:left;
-    margin-left:70px;
-    font-size:20px;
-  }
-  
-  .bell{
-    float:right;
-    margin-right:70px;
-  } 
-   
-   
-  .guarantee{
-     font-size:18px;
-    float:right;
-    margin-right:30px;
-  }
+.el-aside {
+  background-color: #272a2d;
+  color: #333;
+  /* background-image: url('../assets/back.jpeg'); */
+  text-align: center;
+  line-height: 200px;
+  min-height: 90vh;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 30px;
+  min-height: 90vh;
+}
+
+.item {
+  background-color: #272a2d;
+  color: white;
+}
+.system-name {
+  float: left;
+  margin-left: 70px;
+  font-size: 20px;
+}
+
+.bell {
+  float: right;
+  margin-right: 70px;
+}
+
+.guarantee {
+  font-size: 18px;
+  float: right;
+  margin-right: 30px;
+}
 </style>
